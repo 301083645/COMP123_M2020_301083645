69ef2bed4d5c50c07d1a98c35895667760490e2e
301083645 <67066443+301083645@users.noreply.github.com>
2020-06-18 20:59:15 -0400

    초기 커밋.

---
+++ COMP123_M2020_301083645/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_M2020_301083645
+{
+    class Person
+    {
+        // Private Instance variables (fields)
+        private string m_name;
+        private int m_age;
+
+        // Public properties
+        public string Name
+        {
+            get //accessor
+            {
+                return this.m_name;
+            }
+            set // mutator
+            {
+                m_name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.m_age;
+            }
+            set
+            {
+                m_age = value;
+            }
+        }
+
+        //Constructor
+        Person(string name, int age)
+        {
+            m_name = name;
+            m_age = age;
+        }
+        //Private Methods
+        //Public Methods
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello");
+        }
+    }
+}
